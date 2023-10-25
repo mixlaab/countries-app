2a6dc97fe726d1c8265f53b22eb1c2fef62461e7
Víctor Medrano <mixlaab.labs@gmail.com>
2023-10-25 03:05:36 -0600

    Use setState's function approach
    * Use the function approach recommended by React to call the setState
      method. As you can see setState is called with two functions as
      arguments
    * Add didactic comments to explain the best way to work with setState

--- src/App.js
+++ src/App.js
@@ -26,9 +26,26 @@ class App extends Component {
             Hi {firstName} {lastName}! Greetings to {country}
           </p>
           <button
+            /*1st argument: You can pass setState a function that
+            returns an object to achieve the same shallow merge as
+            before. However, with this approach, you gain a level
+            of control and predictability over the asynchronous behavior.
+
+            2nd argument: You can also pass a second callback function
+            as an argument, which runs after the state has been fully
+            updated, offering more precise control over the order of
+            execution.
+            */
             onClick={() => {
+              /* Optimal way we should write our setState code in class
+              components: Using functions on both arguments */
               this.setState(
-                { name: { firstName: 'Paul', lastName: 'McCartney' } },
+                (_state, _props) => ({
+                  name: {
+                    firstName: 'Paul',
+                    lastName: 'McCartney',
+                  },
+                }),
                 () => console.log(this.state)
               );
             }}
