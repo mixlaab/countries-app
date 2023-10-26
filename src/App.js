fae5f252019a79f860b5c0dda7e6700fa16bbce1
Víctor Medrano <mixlaab.labs@gmail.com>
2023-10-26 02:45:29 -0600

    Use array and map method to avoid repetitive code
    * Create an array called countries inside our state, containing all
      country objects
    * Use map method to iterate over the countries array and show an h1 tag
      with country's name for each country in the array

--- src/App.js
+++ src/App.js
@@ -5,24 +5,26 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      country1: {
-        name: "Mexico"
-      },
-      country2: {
-        name: "England"
-      },
-      country3: {
-        name: "Uruguay"
-      },
+      countries: [
+        {
+          name: "Mexico"
+        },
+        {
+          name: "England"
+        },
+        {
+          name: "Uruguay"
+        }
+      ]
     }
   }
 
   render() {
     return (
       <div className="App">
-        <h1>{this.state.country1.name}</h1>
-        <h1>{this.state.country2.name}</h1>
-        <h1>{this.state.country3.name}</h1>
+        {this.state.countries.map((country) => {
+          return <h1>{country.name}</h1>
+        })}
       </div>
     );
   }
