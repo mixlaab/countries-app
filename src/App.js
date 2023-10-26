bdfe8fc69359633ccf29c6c26e5457a6ca64671c
Víctor Medrano <mixlaab.labs@gmail.com>
2023-10-26 02:24:23 -0600

    Show name of three different countries
    * Define individual country objects within component's state, each with
      its own name
    * Use individual JSX tags to show the name of each country

--- src/App.js
+++ src/App.js
@@ -4,12 +4,25 @@ import { Component } from 'react';
 class App extends Component {
   constructor() {
     super();
-    this.state = {}
+    this.state = {
+      country1: {
+        name: "Mexico"
+      },
+      country2: {
+        name: "England"
+      },
+      country3: {
+        name: "Uruguay"
+      },
+    }
   }
 
   render() {
     return (
       <div className="App">
+        <h1>{this.state.country1.name}</h1>
+        <h1>{this.state.country2.name}</h1>
+        <h1>{this.state.country3.name}</h1>
       </div>
     );
   }
